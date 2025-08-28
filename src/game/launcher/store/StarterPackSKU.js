799196424e3f9d0e45b4ce442c21be41f9130865
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2025-08-27 20:22:16 -0400

    update wallet balances gas reserve

--- src/game/launcher/store/StarterPackSKU.js
+++ src/game/launcher/store/StarterPackSKU.js
@@ -305,7 +305,7 @@ const useStarterPackPricing = () => {
     const introMinPrice = priceHelper.from(introPackPriceUSD * TOKEN_SCALE[TOKEN.USDC], TOKEN.USDC);
     const introMinSwayValue = priceHelper.from(introPackSwayMin * TOKEN_SCALE[TOKEN.SWAY], TOKEN.SWAY);
     const introCrewmatesValue = priceHelper.from(introPackCrewmates * adalianPrice.usdcValue, TOKEN.USDC);
-    const introEthValue = priceHelper.from(wallet?.shouldMaintainEthGasReserve ? GAS_BUFFER_VALUE_USDC : 0, TOKEN.USDC);
+    const introEthValue = priceHelper.from(wallet?.shouldMaintainGasReserve ? GAS_BUFFER_VALUE_USDC : 0, TOKEN.USDC);
     const introSwayValue = priceHelper.from(
       Math.max(
         introMinSwayValue.usdcValue,
@@ -318,7 +318,7 @@ const useStarterPackPricing = () => {
     const basicMinPrice = priceHelper.from(basicPackPriceUSD * TOKEN_SCALE[TOKEN.USDC], TOKEN.USDC);
     const basicMinSwayValue = priceHelper.from(basicPackSwayMin * TOKEN_SCALE[TOKEN.SWAY], TOKEN.SWAY);
     const basicCrewmatesValue = priceHelper.from(basicPackCrewmates * adalianPrice.usdcValue, TOKEN.USDC);
-    const basicEthValue = priceHelper.from(wallet?.shouldMaintainEthGasReserve ? GAS_BUFFER_VALUE_USDC : 0, TOKEN.USDC);
+    const basicEthValue = priceHelper.from(wallet?.shouldMaintainGasReserve ? GAS_BUFFER_VALUE_USDC : 0, TOKEN.USDC);
     const basicSwayValue = priceHelper.from(
       Math.max(
         basicMinSwayValue.usdcValue,
@@ -375,7 +375,7 @@ const useStarterPackPricing = () => {
 
 export const StarterPack = ({
   packLabel,
-  shouldMaintainEthGasReserve = false,
+  shouldMaintainGasReserve = false,
   ...props
 }) => {
   const { execute } = useContext(ChainTransactionContext);
@@ -522,7 +522,7 @@ export const StarterPack = ({
               <CrewmateCreditIcon />
               <span style={{ marginLeft: 8 }}>{pack.crewmates} Crewmate{pack.crewmates === 1 ? '' : 's'}</span>
             </SKUHighlight>
-            {shouldMaintainEthGasReserve && (
+            {shouldMaintainGasReserve && (
               <SKUHighlight color={color}>
                 <UserPrice
                   price={pack.ethValue.usdcValue}
