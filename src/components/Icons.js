705f28492b1de618e7cb6d9f8f97db425f1b75bc
Chvx <11530620+Chvx@users.noreply.github.com>
2026-03-05 21:16:16 +0100

    Tank farm icon support and bi-packing logic refactor

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -216,6 +216,7 @@ import RefineryBuildingSVG from '~/assets/icons/buildings/Refinery.svg';
 import ShipyardBuildingSVG from '~/assets/icons/buildings/Shipyard.svg';
 import SpaceportBuildingSVG from '~/assets/icons/buildings/Spaceport.svg';
 import WarehouseBuildingSVG from '~/assets/icons/buildings/Warehouse.svg';
+import TankfarmBuildingSVG from '~/assets/icons/buildings/Tankfarm.svg';
 
 import FlagMarkerSVG from '~/assets/icons/scene/Flag.svg';
 import RouteValidSVG from '~/assets/icons/scene/RouteValidIcon.svg';
@@ -418,6 +419,7 @@ export const RefineryBuildingIcon = () => <RefineryBuildingSVG className="icon" 
 export const ShipyardBuildingIcon = () => <ShipyardBuildingSVG className="icon" />;
 export const SpaceportBuildingIcon = () => <SpaceportBuildingSVG className="icon" />;
 export const WarehouseBuildingIcon = () => <WarehouseBuildingSVG className="icon" />;
+export const TankfarmBuildingIcon = () => <TankfarmBuildingSVG className="icon" />;
 
 export const ResourceGroupIcons = {
   rareearth: (<svg viewBox="0 0 24 24"><path d="M16.67,4H15V2H9V4H7.33A1.33,1.33 0 0,0 6,5.33V20.66C6,21.4 6.6,22 7.33,22H16.66C17.4,22 18,21.4 18,20.67V5.33C18,4.6 17.4,4 16.67,4M11,20V14.5H9L13,7V12.5H15" /></svg>),
